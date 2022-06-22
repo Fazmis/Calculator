c86ab840c5a93f687e3b09ffd2ec9d87b275d203
Fazmis <vlad.par.2004@gmail.com>
2022-06-22 21:24:06 +0300

    Простой консольный калькулятор

---
+++ main.py
@@ -0,0 +1,24 @@
+def calc():
+    num1 = int(input('\nПервое число: '))
+    action = input('Действие: ')
+    num2 = int(input('Второе число: '))
+    action_dict = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y
+    }
+
+    if action in action_dict:
+        print(f'Итого: {action_dict[action](num1, num2)}\n')
+        if input('Жми Q, чтобы завершить: ') in ['Q', 'q']:
+            return
+        else:
+            calc()
+    else:
+        print('Неверное действие!')
+        calc()
+
+
+print('Вас приветствует простой калькулятор :3')
+calc()
