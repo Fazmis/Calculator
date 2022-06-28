d39c95fcb1c1487e43c003ae65ef1d2d836489db
Fazmis <vlad.par.2004@gmail.com>
2022-06-28 15:36:52 +0300

    Почти доделана фича с интерфейсом (ожидает доработок)

--- main.py
+++ main.py
@@ -1,24 +1,84 @@
-def calc():
-    num1 = int(input('\nПервое число: '))
-    action = input('Действие: ')
-    num2 = int(input('Второе число: '))
-    action_dict = {
-        '+': lambda x, y: x + y,
-        '-': lambda x, y: x - y,
-        '*': lambda x, y: x * y,
-        '/': lambda x, y: x / y
-    }
+import tkinter
 
-    if action in action_dict:
-        print(f'Итого: {action_dict[action](num1, num2)}\n')
-        if input('Жми Q, чтобы завершить: ') in ['Q', 'q']:
-            return
-        else:
-            calc()
-    else:
-        print('Неверное действие!')
+# Constants
+WIDTH, HEIGHT = 235, 265
+
+# MainWindow and Widgets
+root = tkinter.Tk()
+root.geometry(f'{WIDTH}x{HEIGHT}')
+root.title('Calculator')
+root.resizable(width=False, height=False)
+root['bg'] = 'Black'
+entry = tkinter.Entry(root, font=('Times New Romance', 15), justify='right')
+
+
+def create_digit_button(digit):
+    return tkinter.Button(root, height=2, text=digit, command=lambda: add_digit(digit),
+                          font=('Times New Romance', 12), bg='Black', fg='Orange')
+
+
+def create_operand_button(operand):
+    return tkinter.Button(root, height=2, text=operand, command=lambda: add_operand(operand),
+                          font=('Times New Romance', 12), bg='Black', fg='Red')
+
+
+def create_c_button(operand):
+    return tkinter.Button(root, height=2, text=operand, command=lambda: c_action(),
+                          font=('Times New Romance', 12), bg='Black', fg='Red')
+
+
+def c_action():
+    entry.delete(0, 'end')
+    value = '0'
+    entry.insert(0, value)
+
+
+def add_digit(num):
+    value = entry.get()
+    entry.delete(0, 'end')
+    value = value + num
+    entry.insert('end', value)
+
+
+def add_operand(operand):
+    if operand == '=':
         calc()
+    else:
+        value = entry.get()
+        entry.delete(0, 'end')
+        if value[-1] in '+-*/':
+            value = value[0:-1] + operand
+        else:
+            value = value + operand
+        entry.insert('end', value)
 
 
-print('Вас приветствует простой калькулятор :3')
-calc()
+def calc():
+    value = entry.get()
+    entry.delete(0, 'end')
+    if value[-1] in '+-*/':
+        value = eval(value + value[:-1])
+    else:
+        value = eval(value)
+    entry.insert('end', value)
+
+
+entry.grid(row=0, columnspan=4, sticky='we', padx=5, pady=5)
+create_digit_button('0').grid(row=4, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('1').grid(row=3, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('2').grid(row=3, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('3').grid(row=3, column=2, sticky='nswe', padx=3, pady=3)
+create_digit_button('4').grid(row=2, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('5').grid(row=2, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('6').grid(row=2, column=2, sticky='nswe', padx=3, pady=3)
+create_digit_button('7').grid(row=1, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('8').grid(row=1, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('9').grid(row=1, column=2, sticky='nswe', padx=3, pady=3)
+create_operand_button('+').grid(row=4, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('-').grid(row=3, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('*').grid(row=2, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('/').grid(row=1, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('=').grid(row=4, column=2, sticky='nswe', padx=3, pady=3)
+create_c_button('C').grid(row=4, column=0, sticky='nswe', padx=3, pady=3)
+
+root.mainloop()
