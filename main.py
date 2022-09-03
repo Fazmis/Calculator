0324828f4185da8bb323131462d8c0f40889b404
Fazmis <vlad.par.2004@gmail.com>
2022-09-03 21:51:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter
 
 # Constants
-WIDTH, HEIGHT = 235, 265
+WIDTH, HEIGHT = 233, 312
 
 # MainWindow and Widgets
 root = tkinter.Tk()
@@ -20,11 +20,11 @@ def create_digit_button(digit):
 
 def create_operand_button(operand):
     return tkinter.Button(root, height=2, text=operand, command=lambda: add_operand(operand),
-                          font=('Times New Romance', 12), bg='Black', fg='Red')
+                          font=('Times New Romance', 10), bg='Black', fg='Red')
 
 
-def create_c_button(operand):
-    return tkinter.Button(root, height=2, text=operand, command=lambda: c_action(),
+def create_special_button(operand, command):
+    return tkinter.Button(root, height=2, text=operand, command=lambda: command(),
                           font=('Times New Romance', 12), bg='Black', fg='Red')
 
 
@@ -50,18 +50,27 @@ def add_operand(operand):
     else:
         value = entry.get()
         entry.delete(0, 'end')
-        if value[-1] in '+-*/':
-            value = value[0:-1] + operand
+        if value[-1] in '+-**/' and value[-1] not in '()':
+            value = value[:-1] + operand
         else:
             value = value + operand
         entry.insert('end', value)
+
+
+def negative():
+    value = entry.get()
+    entry.delete(0, 'end')
+    entry.insert(0, eval(value + '*(-1)'))
 
 
 def calc():
     value = entry.get()
     entry.delete(0, 'end')
     if value[-1] in '+-*/':
-        value = value + value[:-1]
+        if value[-2:] == '**':
+            value = value + '2'
+        else:
+            value = value + value[:-1]
     try:
         entry.insert('end', eval(value))
     except (ZeroDivisionError, SyntaxError, NameError):
@@ -69,21 +78,25 @@ def calc():
 
 
 entry.grid(row=0, columnspan=4, sticky='we', padx=5, pady=5)
-create_digit_button('0').grid(row=4, column=1, sticky='nswe', padx=3, pady=3)
-create_digit_button('1').grid(row=3, column=0, sticky='nswe', padx=3, pady=3)
-create_digit_button('2').grid(row=3, column=1, sticky='nswe', padx=3, pady=3)
-create_digit_button('3').grid(row=3, column=2, sticky='nswe', padx=3, pady=3)
-create_digit_button('4').grid(row=2, column=0, sticky='nswe', padx=3, pady=3)
-create_digit_button('5').grid(row=2, column=1, sticky='nswe', padx=3, pady=3)
-create_digit_button('6').grid(row=2, column=2, sticky='nswe', padx=3, pady=3)
-create_digit_button('7').grid(row=1, column=0, sticky='nswe', padx=3, pady=3)
-create_digit_button('8').grid(row=1, column=1, sticky='nswe', padx=3, pady=3)
-create_digit_button('9').grid(row=1, column=2, sticky='nswe', padx=3, pady=3)
-create_operand_button('+').grid(row=4, column=3, sticky='nswe', padx=3, pady=3)
-create_operand_button('-').grid(row=3, column=3, sticky='nswe', padx=3, pady=3)
-create_operand_button('*').grid(row=2, column=3, sticky='nswe', padx=3, pady=3)
-create_operand_button('/').grid(row=1, column=3, sticky='nswe', padx=3, pady=3)
-create_operand_button('=').grid(row=4, column=2, sticky='nswe', padx=3, pady=3)
-create_c_button('C').grid(row=4, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('0').grid(row=5, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('1').grid(row=4, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('2').grid(row=4, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('3').grid(row=4, column=2, sticky='nswe', padx=3, pady=3)
+create_digit_button('4').grid(row=3, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('5').grid(row=3, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('6').grid(row=3, column=2, sticky='nswe', padx=3, pady=3)
+create_digit_button('7').grid(row=2, column=0, sticky='nswe', padx=3, pady=3)
+create_digit_button('8').grid(row=2, column=1, sticky='nswe', padx=3, pady=3)
+create_digit_button('9').grid(row=2, column=2, sticky='nswe', padx=3, pady=3)
+create_operand_button('(').grid(row=1, column=0, sticky='nswe', padx=3, pady=3)
+create_operand_button(')').grid(row=1, column=1, sticky='nswe', padx=3, pady=3)
+create_operand_button('**').grid(row=1, column=2, sticky='nswe', padx=3, pady=3)
+create_special_button('neg', negative).grid(row=1, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('+').grid(row=5, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('-').grid(row=4, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('*').grid(row=3, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('/').grid(row=2, column=3, sticky='nswe', padx=3, pady=3)
+create_operand_button('=').grid(row=5, column=2, sticky='nswe', padx=3, pady=3)
+create_special_button('C', c_action).grid(row=5, column=0, sticky='nswe', padx=3, pady=3)
 
 root.mainloop()
